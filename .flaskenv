6b9575e280a5bbdc065e83b87ca05307386e959b
SSKInD10 <sskind10@pm.me>
2020-01-12 18:28:49 +0530

    Added Login Page

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=labyrinth.py
+FLASK_ENV=development
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,17 @@
 from app import app
+from flask import render_template,flash,redirect,url_for
+from app.forms import LoginForm
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hello World!"
+@app.route('/home')
+def home():
+    user1 = {'username':'Admin'}
+    return render_template('home.html',title='Index',user=user1)
+
+@app.route('/login',methods = ['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Sign-In', form=form)
